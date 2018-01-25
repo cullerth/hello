ff2fac9e0465616e2c5681ff11b77c9969b82cac
Tori Culler <35739534+cullerth@users.noreply.github.com>
2018-01-25 14:20:12 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 for num in range(3):
     print("It's tori whaddup.")
     print("I was born in March.")
+    print("My fave food is sushi.")
     print("It's sam dood.")
     print("I was born in November.)
