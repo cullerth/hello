a7018de422e17f1fbd3d16963628f6f5994aaf09
ssciolla <samgsciolla@gmail.com>
2018-01-25 14:16:26 -0500

    Repeating something

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
-print("It's tori whaddup.")
-print("I was born in March.")
-print("It's sam dood.")
-print("I was born in November.)
+for num in range(3):
+    print("It's tori whaddup.")
+    print("I was born in March.")
+    print("It's sam dood.")
+    print("I was born in November.)
